c115baabe047015157f534f7a4a6e9170ababb44
Logz11 <reinkell@gmatc.matc.edu>
2020-10-04 15:54:35 -0500

    Added input validation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
 
 const app = express();
 
@@ -11,9 +12,20 @@ app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-})
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', "Please enter a valid email").isEmail(),
+        check('password', 'Please enter a password with 6 or mroe characters').isLength({ min: 6 })
+    ],
+ (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    } else {
+        return res.send(req.body);
+    }
+});
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
