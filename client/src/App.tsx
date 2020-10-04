68791a1d89eb978223d8129efb70e23280c8fb4f
Logz11 <reinkell@gmatc.matc.edu>
2020-10-04 17:29:15 -0500

    Completeed Activity 4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ import axios from 'axios';
 const App = () => {
   const [value, setValue] = useState(null);
 
+  //I hope it's alright that I'm using a functional component here,
+  //I only use functional components for my job and I really want to practice if possible.
   useEffect(() => {
     axios.get('http://localhost:5000')
       .then((response) => {
