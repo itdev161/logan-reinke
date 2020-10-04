0be92347926f8f98672427439c4823b0f4344503
Logz11 <reinkell@gmatc.matc.edu>
2020-10-04 17:15:09 -0500

    Fixed error with directory name

--- server.js
+++ server.js
@@ -28,4 +28,5 @@ app.post(
     }
 });
 
-app.listen(3000, () => console.log('Express server running on port 3000'));
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
