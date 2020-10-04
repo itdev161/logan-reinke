fe7103dc15cc1c08df52c86ed4ec79e6c4f68cc3
Logz11 <reinkell@gmatc.matc.edu>
2020-10-04 15:43:27 -0500

    Added post request

--- models/Users.js
+++ models/Users.js
@@ -3,17 +3,17 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: true
   },
   email: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
   },
   password: {
     type: String,
-    required: true,
-  },
+    required: true
+  }
 });
 
 const User = mongoose.model('user', UserSchema);
--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const app = express();
 
 connectDatabase();
 
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
